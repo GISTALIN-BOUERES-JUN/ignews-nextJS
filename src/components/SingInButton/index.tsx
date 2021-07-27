d8f60caf935a8f18bf94d2cc64bc5f2f8c6544fa
Geraldo Istalin <geraldoboueres@gmail.com>
2021-07-27 13:30:11 -0300

    Next Auth

--- src/components/SingInButton/index.tsx
+++ src/components/SingInButton/index.tsx
@@ -1,19 +1,21 @@
 import { FaGithub } from 'react-icons/fa'
 import styles from './styles.module.scss'
 import { FiX } from 'react-icons/fi'
+import { signIn, signOut, useSession } from 'next-auth/client'
 
 
 export function SingInButton() {
 
-    const isUserLoggedIn = true;
-    return isUserLoggedIn ? (
+    const [session] = useSession();
+    return session ? (
         <button
             type="button"
             className={styles.singInButton}
+            onClick={() => signOut()}
 
         >
             <FaGithub color="#04d361" />
-            Geraldo Boueres
+            {session.user.name}
             <FiX color="#737380" className={styles.closeIcon} />
 
         </button>
@@ -23,6 +25,7 @@ export function SingInButton() {
         <button
             type="button"
             className={styles.singInButton}
+            onClick={() => { signIn('github') }}
 
         >
             <FaGithub color="#eba417" />
