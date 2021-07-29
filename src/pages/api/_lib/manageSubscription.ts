adeb178a720aa1058e56c072cd739286e7ee51a7
Geraldo Istalin <geraldoboueres@gmail.com>
2021-07-29 10:39:37 -0300

    Webhooks Events

---
+++ src/pages/api/_lib/manageSubscription.ts
@@ -0,0 +1,49 @@
+import { query as q } from 'faunadb';
+import { fauna } from "../../../services/fauna";
+import { stripe } from '../../../services/stripe';
+
+
+
+export async function saveSubscription(
+    subscriptionId: string,
+    customerId: string,
+) {
+    //search user inside Faunadb
+
+    console.log(subscriptionId, customerId);
+
+    const userRef = await fauna.query(
+        q.Select(
+            "ref",
+            q.Get(
+                q.Match(
+                    q.Index('user_by_stripe_customer_id'), customerId
+                )
+            )
+        )
+
+    )
+
+
+    //save subscription in Faunad
+
+    const subscription = await stripe.subscriptions.retrieve(subscriptionId)
+
+    const subscriptionData = {
+        id: subscription.id,
+        userId: userRef,
+        status: subscription.status,
+        priceId: subscription.items.data[0].price.id,
+
+    }
+
+    await fauna.query(
+        q.Create(
+            q.Collection('subscriptions'),
+            { data: subscriptionData }
+        )
+    )
+
+
+
+}
