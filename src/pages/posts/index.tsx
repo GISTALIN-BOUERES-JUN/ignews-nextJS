ea12fdf958d1c44842a0628a8c47e72a6f1d830f
Geraldo Istalin <geraldoboueres@gmail.com>
2021-07-31 17:45:45 -0300

    Post - Prismic

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -1,8 +1,24 @@
+import { GetStaticProps } from 'next';
 import Head from 'next/head';
 import React from 'react';
+import { getPrismicClient } from '../../services/prismic';
 import styles from './styles.module.scss';
+import Prismic from '@prismicio/client';
+import { RichText } from 'prismic-dom';
 
-export default function Posts() {
+type Post = {
+    slug: string;
+    title: string;
+    excerpt: string;
+    updatedAt: string;
+
+}
+
+interface PostsProps {
+    posts: Post[]
+}
+
+export default function Posts({ posts }: PostsProps) {
     return (
         <>
             <Head>
@@ -13,38 +29,53 @@ export default function Posts() {
 
             <main className={styles.container}>
                 <div className={styles.posts}>
-                    <a>
-                        <time>
-                            12 de março de 2021
-                        </time>
-                        <strong> Creating a monorepo with Lerna Workspaces </strong>
-                        <p> Bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla</p>
 
-                    </a>
+                    {posts.map(post => (
+                        <a key={post.slug} href="#">
+                            <time> {post.updatedAt} </time>
+                            <strong> {post.title} </strong>
+                            <p> {post.excerpt}</p>
 
-                    <a>
-                        <time>
-                            12 de março de 2021
-                        </time>
-                        <strong> Creating a monorepo with Lerna Workspaces </strong>
-                        <p> Bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla</p>
-
-                    </a>
-
-                    <a>
-                        <time>
-                            12 de março de 2021
-                        </time>
-                        <strong> Creating a monorepo with Lerna Workspaces </strong>
-                        <p> Bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla</p>
-
-                    </a>
-
-
+                        </a>
+                    ))}
                 </div>
 
             </main>
 
         </>
     );
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+    const prismic = getPrismicClient()
+
+    const response = await prismic.query([
+        Prismic.Predicates.at('document.type', 'publication')
+    ],
+        {
+            fetch: ['publication.title', 'publication.content'],
+            pageSize: 100,
+        })
+
+    console.log(response)
+
+    const posts = response.results.map(post => {
+        return {
+            //url
+            slug: post.uid,
+            title: RichText.asText(post.data.title),
+            excerpt: post.data.content.find(content => content.type === 'paragraph')?.text ?? '',
+            updatedAT: new Date(post.last_publication_date).toLocaleDateString('pt-BR', {
+                day: '2-digit',
+                month: 'long',
+                year: 'numeric'
+            })
+
+        };
+    });
+
+    return {
+        props: { posts }
+    }
+
 }
